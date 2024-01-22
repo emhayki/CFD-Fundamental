323303e75f0c362f2c4d4f33e435ab7937b2a716
emhayki <135982304+emhayki@users.noreply.github.com>
2024-01-22 06:56:24 +0000

    No commit message

---
+++ Python/step03.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+########################################################
+# Step 3: 1D Diffusion
+########################################################
+
+X  = 41;                    # Number of spatial points
+T  = 20;                    # Number of time steps
+v  = 0.1;                   # Diffusion coefficient
+dx = 2. / (X - 1.);         # Spatial step size
+dt = 0.2 * dx**2 / v;       # Time step size
+
+# Initialise spatial grid and initial condition
+x = np.linspace(0, 2, num=X)
+u = np.ones(X)
+u[(x >= 0.5) & (x <= 1)] = 2
+
+# Setup the plot
+fig, ax = plt.subplots()
+line, = ax.plot(x, u, 'r', linewidth=1)
+ax.set_xlim(0, 2)
+ax.set_ylim(0.5, 2.5)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$u$')
+ax.set_title('1D Diffusion')
+
+def animate(n):
+    global u
+    un = u.copy()  
+    for i in range(1, X-1):  
+        u[i] = v * dt/dx**2 * (un[i+1] - 2*un[i] + un[i-1]) + un[i]
+    line.set_ydata(u)
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, frames=T, interval=50)
+
+plt.show()
+
+
+
